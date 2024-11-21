0162abb8c947588b86149ed29f53f05c7db9d303
yangxing <yangxing844@gmail.com>
2024-11-22 00:42:16 +0800

    add physical layout for aum

--- config/boards/arm/aum/board.cmake
+++ config/boards/arm/aum/board.cmake
@@ -1,3 +1,6 @@
-board_runner_args(nrfprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+# SPDX-License-Identifier: MIT
+
+board_runner_args(pyocd "--target=nrf52840")
+set(OPENOCD_NRF5_INTERFACE "cmsis-dap")
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
