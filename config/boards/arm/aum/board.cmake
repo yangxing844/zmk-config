e927319341c6a16d027745bd5c0753ce16fe5e8b
yangxing <yangxing844@gmail.com>
2024-12-20 03:18:49 +0800

    remove openocd

--- config/boards/arm/aum/board.cmake
+++ config/boards/arm/aum/board.cmake
@@ -3,4 +3,3 @@
 board_runner_args(pyocd "--target=nrf52840")
 set(OPENOCD_NRF5_INTERFACE "cmsis-dap")
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
