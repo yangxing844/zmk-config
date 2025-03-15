19332f2a03d5796e31a54d328c59d97805c407f0
yangxing <yangxing844@gmail.com>
2025-03-16 03:13:31 +0800

    fix planck column pull down resistor, the idle current is now 33ua

--- config/boards/arm/planck/pinmux.c
+++ config/boards/arm/planck/pinmux.c
@@ -15,36 +15,6 @@
 static int pinmux_planck_init(void) {
 
 #if CONFIG_BOARD_PLANCK_LEFT
-  const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-  const struct device *p1 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-  int pin1 = 9;
-  int pin2 = 10;
-
-#if CONFIG_BOARD_CHARGER_CURRENT_40MA
-  gpio_pin_configure(p0, pin1, GPIO_INPUT | GPIO_PULL_DOWN);
-  gpio_pin_configure(p1, pin2, GPIO_INPUT);
-#elif CONFIG_BOARD_CHARGER_CURRENT_100MA
-  gpio_pin_configure(p0, pin1, GPIO_OUTPUT);
-  gpio_pin_set(p0, pin1, 0);
-  gpio_pin_configure(p1, pin2, GPIO_INPUT);
-#elif CONFIG_BOARD_CHARGER_CURRENT_150MA
-  gpio_pin_configure(p0, pin1, GPIO_OUTPUT);
-  gpio_pin_set(p0, pin1, 0);
-  gpio_pin_configure(p1, pin2, GPIO_INPUT | GPIO_PULL_DOWN);
-#elif CONFIG_BOARD_CHARGER_CURRENT_250MA
-  gpio_pin_configure(p0, pin1, GPIO_INPUT);
-  gpio_pin_configure(p1, pin2, GPIO_OUTPUT);
-  gpio_pin_set(p1, pin2, 0);
-#elif CONFIG_BOARD_CHARGER_CURRENT_350MA
-  gpio_pin_configure(p0, pin1, GPIO_OUTPUT);
-  gpio_pin_set(p0, pin1, 0);
-  gpio_pin_configure(p1, pin2, GPIO_OUTPUT);
-  gpio_pin_set(p1, pin2, 0);
-#elif CONFIG_BOARD_CHARGER_CURRENT_NONE
-  gpio_pin_configure(p0, pin1, GPIO_INPUT);
-  gpio_pin_configure(p1, pin2, GPIO_INPUT);
-
-#endif
 
 #elif CONFIG_BOARD_PLANCK_RIGHT
   const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
