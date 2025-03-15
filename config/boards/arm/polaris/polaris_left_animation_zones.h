91c9636fc120d97d46012ce08b7be693e3c24594
yangxing <yangxing844@gmail.com>
2025-03-16 03:13:57 +0800

    trying some rgb effects

---
+++ config/boards/arm/polaris/polaris_left_animation_zones.h
@@ -0,0 +1,32 @@
+// Pixels on the strip are ordered from left to right.
+//
+// ╔════════════════════════════════╗
+// ║ 0 ╲ 1 ╱ 2 ╲ ... ╱ 32 ╲ 33 ╱ 34 ║
+// ╠════════════════════════════════╣
+// ║                                ║
+// ║                                ║
+// ╚═══╗                        ╔═══╝
+//     ╚════════════════════════╝
+
+#define ZONE_STRIP 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34
+
+#define ZONE_STRIP_LEFT 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+#define ZONE_STRIP_CENTER 17
+#define ZONE_STRIP_RIGHT 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34
+
+#define ZONE_INDICATOR_L1 0 1 2
+#define ZONE_INDICATOR_L2 5 6 7
+#define ZONE_INDICATOR_L3 10 11 12
+#define ZONE_INDICATOR_L4 15 16 17
+
+#define ZONE_INDICATOR_L1_BG 0 1 2 3 4
+#define ZONE_INDICATOR_L2_BG 0 1 2 3 4 5 6 7 8 9
+#define ZONE_INDICATOR_L3_BG 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+#define ZONE_INDICATOR_L4_BG 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+
+#define ZONE_INDICATOR_R1 32 33 34 35
+
+#define ZONE_INDICATOR_R1_BG 30 31 32 33 34
+
+#define ZONE_RAINBOW 4 5 6 7 8 9 10 11 12 13 14 15
+#define ZONE_RAINBOW_BORDER 1 2 3 16 17 18
